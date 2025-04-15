b59dd7ad240f83afd452fab28331e69b1fe0a7e8
harrisonhunter <harrison@maestroqa.com>
2025-04-15 10:21:16 -0400

    add some dupes into example data

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /**
  * TODO:
  *
- * 1. Fetch all 7 emails using fetchEmailsFromDatabase
+ * 1. Fetch all emails using fetchEmailsFromDatabase
  * 2. Throw out responses from fetchEmailsFromDatabase with (error === true), and handle retries
- * 3. Render all emails using renderEmails
+ * 3. Render all unique emails using renderEmails, sorted by creation time
  */
 function main() {
   // Your Code Here
@@ -16,8 +16,8 @@ main();
 /**
  * A simulated API call to fetch emails. The pagination
  * length is randomly determined.
- * 
- * @param {number} offset - Offset for pagination 
+ *
+ * @param {number} offset - Offset for pagination
  * @param {function} callback - Has args (error, { emails, next })
  *   error: {boolean}, random error that indicates response should be ignored
  *   emails: {object[]}, results that were fetched from this call
@@ -29,36 +29,55 @@ function fetchEmailsFromDatabase(offset, callback) {
       author: 'Bobby Bob',
       subject: 'Hey Friend!',
       body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+      creation_time: '2023-10-01T12:00:00Z',
     },
     {
       author: 'Bobby Not-Bob',
       subject: 'Hey Friend!',
       body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+      creation_time: '2023-10-01T12:00:00Z',
     },
     {
       author: 'Bobby Obo',
       subject: 'Hey Friendo!',
       body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+      creation_time: '2023-10-01T12:00:00Z',
     },
     {
       author: 'Jenny Jane',
       subject: 'Let me know if you are planning...',
       body: `ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+      creation_time: '2023-10-01T12:00:00Z',
     },
     {
       author: 'Jenny Janey',
       subject: 'Let Jenny know if you are planning...',
       body: `ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+      creation_time: '2023-10-01T12:00:00Z',
     },
     {
       author: 'Some Guy',
       subject: 'Please DO NOT buy my product.',
       body: `My product is a scam. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat quis nostrud.`,
+      creation_time: '2023-10-03T12:00:00Z',
+    },
+    {
+      author: 'Some Guy',
+      subject: 'Please DO NOT buy my product.',
+      body: `My product is a scam. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat quis nostrud.`,
+      creation_time: '2023-10-04T12:00:00Z',
     },
     {
       author: 'Some Guy',
       subject: 'Please buy my product.',
       body: `My product is the best. For just $1,000 you could buy my product and make me somewhat richer. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat quis nostrud.`,
+      creation_time: '2023-10-03T05:00:00Z',}
+    ,
+    {
+      author: 'Jenny Jane',
+      subject: 'Let me know if you are planning...',
+      body: `ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+      creation_time: '2023-10-04T05:00:00Z',
     },
   ];
 
